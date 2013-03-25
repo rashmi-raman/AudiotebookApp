b2451b7a2d956159a5d842b7beeaab5252b310b5
Robert Walport <robertwalport@gmail.com>
2013-03-25 18:54:09 -0400

    added comments and clear attribution to the coredata helper files

--- Audiotebook_v1.0/CoreDataHelper.m
+++ Audiotebook_v1.0/CoreDataHelper.m
@@ -1,3 +1,25 @@
+//
+//  CoreDataHelper.m
+//  PictureList
+//
+//  Created by Simon from Maybelost.com
+//
+//  If you use this code, it'd be awesome if you'd give a mention to my blog
+//  site so that others may also get some use from the tutorials. It's not
+//  mandatory of course, but it'll give you a warm fuzzy feeling inside.
+//
+//  ------------------------------------------------------------------------
+//
+//  MODIFIED version of this...
+//
+//  Created by Björn Sållarp on 2009-06-14.
+//  NO Copyright 2009 MightyLittle Industries. NO rights reserved.
+//
+//  Use this code any way you like. If you do like it, please
+//  link to my blog and/or write a friendly comment. Thank you!
+//
+//  Read my blog @ http://blog.sallarp.com
+
 #import "CoreDataHelper.h"
 
 @implementation  CoreDataHelper
